8ad39ab928a1305b174964acac4decfdc831cd3c
Daniel Toledo <dan992965@gmail.com>
2024-02-02 21:35:01 -0300

    feat: define propriedades da interface IProductRepository

--- ShoppingMicroservice.ProductAPI/Repository/IProductRepository.cs
+++ ShoppingMicroservice.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ShoppingMicroservice.ProductAPI.Repository
+﻿using ShoppingMicroservice.ProductAPI.Data.ValueObjects;
+
+namespace ShoppingMicroservice.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
