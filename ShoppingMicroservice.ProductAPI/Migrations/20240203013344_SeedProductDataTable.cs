501d24ff148f57d3760b81da875161b761c10e1a
Daniel Toledo <dan992965@gmail.com>
2024-02-02 22:36:07 -0300

    feat: adiciona seed para popular database

---
+++ ShoppingMicroservice.ProductAPI/Migrations/20240203013344_SeedProductDataTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingMicroservice.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 3L, "CAMISETA", "A camiseta Nike Sportswear coloca você em um macio tecido jersey de algodão e um logotipo clássico no peito.", "https://imgnike-a.akamaihd.net/768x768/01638851.jpg", "Camiseta Nike Sportswear Icon Futura Masculina", 79.99m },
+                    { 4L, "CAMISETA", "A Coleção da Seleção Brasileira de 2022 combina a icônica estampa da onça-pintada com design inovador que mantém seu corpo seco mesmo no auge da empolgação.", "https://imgnike-a.akamaihd.net/768x768/0226290L.jpg", "Camisa Nike Brasil I 2022/23 Jogador Masculina", 699.99m },
+                    { 5L, "TENIS", "O Invincible Run 3 possui amortecimento espesso para ajudar você a ficar atento e ativo hoje, amanhã e sempre.", "https://imgnike-a.akamaihd.net/768x768/02523151.jpg", "Tênis Nike Invincible 3 Masculino", 899.99m },
+                    { 6L, "MOCHILA", "Carregue seu equipamento com a mochila Nike Heritage.", "https://imgnike-a.akamaihd.net/768x768/012822O6A1.jpg", "Mochila Nike Heritage Unissex\r\n", 279.99m },
+                    { 7L, "BLUSA", "Comece e termine sua aventura ao ar livre com este moletom confortável para corrida em trilha.", "https://imgnike-a.akamaihd.net/768x768/024982NX.jpg", "Blusão Nike Dri-FIT Magic Hour Masculino\r\n", 599.99m },
+                    { 8L, "TENIS", "Camadas sobre camadas de estilo e conforto - essa é uma força a ser reconhecida.", "https://imgnike-a.akamaihd.net/768x768/02567551.jpg", "Tênis Nike Gamma Force Feminino\r\n", 699.99m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 8L);
+        }
+    }
+}
--- ShoppingMicroservice.ProductAPI/Model/Context/MYSQLContext.cs
+++ ShoppingMicroservice.ProductAPI/Model/Context/MYSQLContext.cs
@@ -8,5 +8,68 @@ namespace ShoppingMicroservice.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Camiseta Nike Sportswear Icon Futura Masculina",
+                Price = new decimal(79.99),
+                Description = "A camiseta Nike Sportswear coloca você em um macio tecido jersey de algodão e um logotipo clássico no peito.",
+                ImageURL = "https://imgnike-a.akamaihd.net/768x768/01638851.jpg",
+                CategoryName = "CAMISETA"
+            });            
+            
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Camisa Nike Brasil I 2022/23 Jogador Masculina",
+                Price = new decimal(699.99),
+                Description = "A Coleção da Seleção Brasileira de 2022 combina a icônica estampa da onça-pintada com design inovador que mantém seu corpo seco mesmo no auge da empolgação.",
+                ImageURL = "https://imgnike-a.akamaihd.net/768x768/0226290L.jpg",
+                CategoryName = "CAMISETA"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Tênis Nike Invincible 3 Masculino",
+                Price = new decimal(899.99),
+                Description = "O Invincible Run 3 possui amortecimento espesso para ajudar você a ficar atento e ativo hoje, amanhã e sempre.",
+                ImageURL = "https://imgnike-a.akamaihd.net/768x768/02523151.jpg",
+                CategoryName = "TENIS"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Mochila Nike Heritage Unissex\r\n",
+                Price = new decimal(279.99),
+                Description = "Carregue seu equipamento com a mochila Nike Heritage.",
+                ImageURL = "https://imgnike-a.akamaihd.net/768x768/012822O6A1.jpg",
+                CategoryName = "MOCHILA"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 7,
+                Name = "Blusão Nike Dri-FIT Magic Hour Masculino\r\n",
+                Price = new decimal(599.99),
+                Description = "Comece e termine sua aventura ao ar livre com este moletom confortável para corrida em trilha.",
+                ImageURL = "https://imgnike-a.akamaihd.net/768x768/024982NX.jpg",
+                CategoryName = "BLUSA"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 8,
+                Name = "Tênis Nike Gamma Force Feminino\r\n",
+                Price = new decimal(699.99),
+                Description = "Camadas sobre camadas de estilo e conforto - essa é uma força a ser reconhecida.",
+                ImageURL = "https://imgnike-a.akamaihd.net/768x768/02567551.jpg",
+                CategoryName = "TENIS"
+            });
+        }
     }
 }
